2ad037752b6a67cd768d18f059e473457b3df123
radeexpitheta@gmail.com <radeexpitheta@gmail.com@0161f244-4dfc-50f6-f40d-e3044cf3a87a>
2011-03-30 04:53:42 +0000

    added makeshift lightweight game start - based off of cuberor!!

---
+++ trunk/mods/mazecraft.sdd/ModOptions.lua
@@ -0,0 +1,10 @@
+local options={
+	{
+		key="pointmode",
+		name="Is this game awesome?",
+		desc="This choice in judgemtn will decide if the player is worthy or not",
+		type="bool",
+		def="true",
+	},
+}
+return options
