85537d7f1f7e1040230dbc894306c8bd738896f5
radeexpitheta <radeexpitheta@gmail.com>
2011-03-30 04:53:42 +0000

    added makeshift lightweight game start - based off of cuberor!!

---
+++ mods/mazecraft.sdd/modinfo.lua
@@ -0,0 +1,15 @@
+local modinfo ={
+   name='Mother-fucking Mazecraft',
+  description='Find the exit!',
+  shortname='Mazecraft',
+  version='Alpha 0001',
+  mutator='',
+  game='Mazecraft alpha',
+  shortGame='Mazey',
+  modtype=1,
+        depend = {
+                "cursors.sdz",
+        }
+}
+ 
+return modinfo
